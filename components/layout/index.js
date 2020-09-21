66aac07e4e99b10966043dd8944880bfe4c596f4
Flyken271 <jaredcollins99@gmail.com>
2020-09-21 12:09:01 -0400

    Rebranded logos

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,10 +1,20 @@
 import Navbar1 from "@/components/Navbar";
 import Footer1 from "@/components/Footer1";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar1 navbarBrand="WePost" />
+      <Head>
+        <title>WePost - Freelancing</title>
+        <meta
+          name="description"
+          content="WePost, post job offers for freelancers to complete."
+        />
+      </Head>
+      <Navbar1
+        navbarBrand={<img src="/logo192.png" width="350" height="150" />}
+      />
       <main>{children}</main>
       <Footer1 />
     </>
